4c7d05a8386c535866526e30d968ca3abbb252e7
Devonta Harris <devontah94@gmail.com>
2015-09-22 22:31:15 -0400

    Designed Welcome view

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,13 @@
-angular.module('Space', ['ngResource']);
+angular.module('Space', ['ngRoute'])
+	.config(function($routeProvider, $locationProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: '/js/views/welcome.html'
+			})
+			.when('/spaces', {
+				templateUrl: '/js/views/spaces.html',
+				controllerAs: 'MainCtrl as main'
+			}); 
+
+		$locationProvider.html5Mode(true);
+	});
