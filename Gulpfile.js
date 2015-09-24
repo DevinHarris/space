cbc190b7245946b2371ffea517eec205776c851f
Devonta Harris <devontah94@gmail.com>
2015-09-24 00:29:17 -0400

    Added gulp-livereload

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp'),
-	sass = require('gulp-sass');
+	sass = require('gulp-sass'),
+	livereload = require('gulp-livereload');
 
 
 gulp.task('sass', function() {
 	gulp.src('./public/css/*.scss')
 		.pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-		.pipe(gulp.dest('./public/css'));
+		.pipe(gulp.dest('./public/css'))
+		.pipe(livereload());
+});
+
+gulp.task('reload', function() {
+	gulp.src('./public/index.html')
+		.pipe(livereload());
 });
 
 gulp.task('default', function() {
 	gulp.watch('./public/css/*.scss', ['sass']);
+	gulp.watch('./public.index.html', ['reload']);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('Space', ['ngRoute'])
+angular.module('Space', ['ngRoute', 'ngAnimate'])
 	.config(function($routeProvider, $locationProvider) {
 		$routeProvider
 			.when('/', {
